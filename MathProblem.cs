b94df0b70028f450181550def1f1427d37b2cf27
SpeedummD <146203148+SpeedummD@users.noreply.github.com>
2023-11-18 11:19:08 +0200

    Math Problem Square Block (Not optimized)
    In a map, there are two roads. Each road can be divided into multiple segments of integer length. Write a piece of code that finds a number that represents the largest  size of the segment in a square city block created from the two roads. If not possible, the code returns 0.
    Examples:
    1) Given A = 10, B = 21. The solution is 7. The second road is split into three segments of length 7 and the first is cut into one segment of length 7, and the segment of length 3 is ignored.
    2) Given A = 13, B = 11. The solution is 5. You can get two segments of length 5 from each road.
    
    Make the solution as efficient as possible.

---
+++ MathProblem.cs
@@ -0,0 +1,50 @@
+﻿namespace MathProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            var sum = (a + b) / 4;
+
+
+            if (sum <= 0)
+            {
+                Console.WriteLine(0);
+            }
+
+            else if (a < sum || b < sum)
+            {
+                Console.WriteLine(Math.Min(a, b));
+            }
+
+            else
+            {
+                bool rule = true;
+
+                while (rule)
+                {
+                    var tempSum = (a / sum) + (b / sum);
+
+                    if (sum == 0)
+                    {
+                        Console.WriteLine(sum);
+                        rule = false;
+                    }
+                    else if (tempSum == 4)
+                    {
+                        Console.WriteLine(sum);
+                        rule = false;
+                    }
+
+                    else
+                    {
+                        sum--;
+                    }
+                }
+            }
+        }
+    }
+}
